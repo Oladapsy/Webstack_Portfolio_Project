f61d5179fab62c123a07fe6387b0220dbc1384a0
olaguru <dapoodedeyi03@gmail.com>
2024-12-11 12:41:07 +0100

    added address to the Client model in invoice app

--- invoices/models.py
+++ invoices/models.py
@@ -5,6 +5,7 @@ class Client(models.Model):
     """model for the user or client"""
     company_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    address = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.company_name} ({self.email})"
